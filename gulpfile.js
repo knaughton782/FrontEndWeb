61dd1b78c81515db57e10d0a0c293a604e132829
Kirsten Naughton <kirsten.naughton@gmail.com>
2017-08-25 15:40:39 -0700

    updates to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
 
 gulp.task("sass", function() {
   return gulp
@@ -13,8 +15,32 @@ gulp.task("sass", function() {
     .pipe(gulp.dest("public/css"));
 });
 gulp.task("watch", function() {
-  return gulp.watch("styles/**/*.scss", ["sass"]);
+  return gulp.watch(
+    ["styles/**/*.scss", "public/javascript/**/*.js"][("sass", "build")]
+  );
 });
 gulp.task("default", ["sass"], function() {
   return;
 });
+gulp.task("concat", function() {
+  return gulp
+    .src([
+      "public/javascript/**/*.js",
+      "public/libs/materialize/dist/js/materialize.js",
+      "public/libs/jquery/dist/jquery.js"
+    ])
+    .pipe(concat("app.bundle.js"))
+    .pipe(gulp.dest("public/dist"));
+});
+gulp.task("minify", function() {
+  return gulp
+    .src("public/dist/app.bundle.js")
+    .pipe(
+      uglify({
+        mangle: true
+      })
+    )
+    .pipe(rename("app.bundle.min.js"))
+    .pipe(gulp.dest("public/dist"));
+});
+gulp.task("build", ["concat", "minify"]);
