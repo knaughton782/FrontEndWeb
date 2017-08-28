23a5a625880ed9ea110894998baa703787ab7dbf
Kirsten Naughton <kirsten.naughton@gmail.com>
2017-08-28 07:43:12 -0700

    working index and favorites, header, footer on all pages

--- public/javascript/jquery.js
+++ public/javascript/jquery.js
@@ -1,9 +1,3 @@
 $(function() {
-<<<<<<< HEAD
   $(".slider").slider();
-=======
-  $("button").click(function() {
-    $("ul dropdown").hide();
-  });
->>>>>>> a0db20f62529ec708fd9276dffe30a8c920c17ac
 });
